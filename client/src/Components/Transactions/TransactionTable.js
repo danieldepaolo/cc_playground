7c6aa86e0474b7e795798ab2c6d45f3c55f2a490
Daniel DePaolo <djdep13@gmail.com>
2018-10-28 09:41:32 -0700

    Add TransactionTable component that was missing.
    I forgot to add this to previous commit.

---
+++ client/src/Components/Transactions/TransactionTable.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactTable from "react-table";
+
+const columns = [{
+  Header: 'Date',
+  accessor: 'date',
+  width: 100
+}, {
+  Header: 'Merchant',
+  accessor: 'merchant',
+  filterable: true
+}, {
+  Header: 'Product Type',
+  accessor: 'productType',
+  filterable: true
+}, {
+  Header: 'Amount',
+  accessor: 'amount',
+  width: 80
+}];
+
+const TransactionTable = ( ({transactions}) => {
+  return (
+    <div className="tableContainer">
+      <ReactTable
+        columns={columns}
+        data={transactions}
+        defaultPageSize={20}
+      />
+    </div>
+  );
+});
+
+export default TransactionTable;
