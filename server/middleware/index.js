95b5e771a115811bcd77d13fb11620e66cec954e
Daniel DePaolo <djdep13@gmail.com>
2025-07-28 13:25:20 -0700

    Convert middleware to use async/await

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,26 +1,30 @@
 const Card = require('../models/card');
 
-function userCreatedCard(req, res, next) {
+async function userCreatedCard(req, res, next) {
   const { id } = req.params;
   console.log(req.user._id);
 
-  Card.findOne({_id: id}, (err, foundCard) => {
-    if (err) {
-      return res.json({error: err});
-    }
-    if (!foundCard) {
-      return res.json({error: "Did not find card in database!"});
-    }
+  let foundCard, error;
 
-    const hasContributor = !!foundCard.contributor.id;
-    // Normally we want the card to only be editable by the user who created it
+  try {
+    foundCard = await Card.findOne({_id: id});
+  } catch (err) {
+    return res.json({ error: err });
+  }
+
+  if (!foundCard) {
+    return res.json({error: "Did not find card in database!"});
+  }
+
+  const hasContributor = !!foundCard.contributor.id;
+
+  // Normally we want the card to only be editable by the user who created it
     // Turn back on if needed
     if (true/*!hasContributor || foundCard.contributor.id.equals(req.user._id)*/) {
       return next();
     } else {
       return res.json({error: "You do not have permission to do this."});
     }
-  });
 }
 
 module.exports = {
